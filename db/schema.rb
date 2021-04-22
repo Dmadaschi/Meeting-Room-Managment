1a5149a10450bbab547bf96c7a84789fdd2de68c
Daniel Madaschi <nunean@gmail.com>
2021-04-22 16:35:22 -0300

    change database sqlite3 to postegresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,8 +12,11 @@
 
 ActiveRecord::Schema.define(version: 2021_04_22_130710) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "reservations", force: :cascade do |t|
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "reservation_datetime"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
